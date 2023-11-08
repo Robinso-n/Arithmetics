e3b2d15982c6bcf39775a85868ca5a6813b33d9a
Robinso-n <133510190+Robinso-n@users.noreply.github.com>
2023-11-08 13:31:05 +0300

    area of triangle class

---
+++ AreOfTriangle.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class AreOfTriangle {
+	double area,height,base;
+	public void area(double area,double height,double base) {
+		this.area=area;
+		this.height=height;
+		this.base=base;
+	}
+	public void area(double base,double height ) {
+		double area=(0.5*base*height);
+		System.out.println("Area= "+area);
+		
+	}
+
+}
