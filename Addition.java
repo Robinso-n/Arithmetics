545c22869373e055ad3d835eaf521cf99673dca3
Barlion <bravinbarlion01@gmail.com>
2023-11-08 11:08:38 +0300

    Create Addition.java
    Addition class

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+    private int int1;
+    private int int2;
+
+    public Addition(int int1, int int2) {
+        this.int1 = int1;
+        this.int2 = int2;
+    }
+
+    public int calculateSum() {
+        return int1 + int2;
+    }
+}
