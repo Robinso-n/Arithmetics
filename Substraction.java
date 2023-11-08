0da5337070b8dd8707e0464c7388c23017cc75f7
Barlion <bravinbarlion01@gmail.com>
2023-11-08 11:26:16 +0300

    Addition and Substraction

---
+++ Substraction.java
@@ -0,0 +1,13 @@
+public class Substraction {
+    private int int1;
+    private int int2;
+
+    public Substraction(int int1, int int2) {
+        this.int1 = int1;
+        this.int2 = int2;
+    }
+
+    public int calculateSubstract() {
+        return int1 - int2;
+    }
+}
